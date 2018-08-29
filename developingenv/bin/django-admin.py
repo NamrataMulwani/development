3a74a35d7abf962b8eed6cb6a2d3c219ec61f901
Namrata Ashok Mulwani <namratamulwani@Namratas-MacBook-Air.local>
2018-08-30 02:35:58 +0530

    Initial Heroku commit

--- developingenv/bin/django-admin.py
+++ developingenv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/namratamulwani/Desktop/Development/developingenv/bin/python
+#!/Users/namratamulwani/Desktop/Development/developingenv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
